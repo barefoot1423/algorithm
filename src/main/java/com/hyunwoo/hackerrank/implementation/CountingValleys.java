832f56e28e4640844f244545b776e48b5c33c21d
Hyunwoo Cho <barefoot1423@gmail.com>
2020-01-27 23:18:52 -0500

    done CountingValleys

--- src/main/java/com/hyunwoo/hackerrank/implementation/CountingValleys.java
+++ src/main/java/com/hyunwoo/hackerrank/implementation/CountingValleys.java
@@ -2,40 +2,26 @@ package com.hyunwoo.hackerrank.implementation;
 
 //https://www.hackerrank.com/challenges/counting-valleys/problem
 
-import org.omg.CORBA.INTERNAL;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 public class CountingValleys {
 
     public int countingValleys(int n, String s) {
 
         int vallyCount = 0;
-        int sum = 0;
+        int altitude = 0;
 
-        int[] ints = Stream.of(s.split("")).mapToInt(CountingValleys::parse).toArray();
+        for (char ch : s.toCharArray()) {
+            if (ch == 'U') {
+                altitude++;
 
-        for (int i = 1; i < ints.length; i++) {
-            sum += ints[i - 1];
-
-            if (sum == 0 && ints[i - 1] == -1) {
-                vallyCount++;
+                if (altitude == 0) {
+                    vallyCount++;
+                }
+            } else {
+                altitude--;
             }
         }
 
-
         return vallyCount;
-    }
-
-    private static Integer parse(String s) {
-        if (s.equals("U")) {
-            return 1;
-        } else {
-            return -1;
-        }
     }
 
 }
