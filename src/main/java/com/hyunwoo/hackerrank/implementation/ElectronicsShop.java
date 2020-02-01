700b40d2b010404f5d97d48c2bb8412d365fd830
Hyunwoo Cho <barefoot1423@gmail.com>
2020-01-31 22:26:05 -0500

    start ElectronicsShop

---
+++ src/main/java/com/hyunwoo/hackerrank/implementation/ElectronicsShop.java
@@ -0,0 +1,13 @@
+package com.hyunwoo.hackerrank.implementation;
+
+// https://www.hackerrank.com/challenges/electronics-shop/problem
+
+
+public class ElectronicsShop {
+
+    public int getMoneySpent(int[] keyboards, int[] drives, int b) {
+
+
+        return 0;
+    }
+}
