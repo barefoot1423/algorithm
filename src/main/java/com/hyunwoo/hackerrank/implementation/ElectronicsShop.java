20d061be4c158c4533699fe812bf7781f7352a43
Hyunwoo Cho <barefoot1423@gmail.com>
2020-02-02 23:40:49 -0500

    done ElectronicsShop

--- src/main/java/com/hyunwoo/hackerrank/implementation/ElectronicsShop.java
+++ src/main/java/com/hyunwoo/hackerrank/implementation/ElectronicsShop.java
@@ -3,11 +3,29 @@ package com.hyunwoo.hackerrank.implementation;
 // https://www.hackerrank.com/challenges/electronics-shop/problem
 
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ElectronicsShop {
 
     public int getMoneySpent(int[] keyboards, int[] drives, int b) {
 
+        List<Integer> keyboardList = Arrays.stream(keyboards).boxed().sorted().collect(Collectors.toList());
+        List<Integer> driveList = Arrays.stream(drives).boxed().collect(Collectors.toList());
+        int max = -1;
 
-        return 0;
+        for (Integer keyboard : keyboardList) {
+            for (Integer drive : driveList) {
+                int sum = keyboard + drive;
+                if (sum <= b && sum > max) {
+                    max = sum;
+                }
+            }
+        }
+
+        return max;
     }
+
 }
+
