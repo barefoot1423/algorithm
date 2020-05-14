60c5e2eceeaf27ef8c5bdebfdffa6b7589094122
Hyunwoo Cho <barefoot1423@gmail.com>
2020-05-14 00:23:11 -0400

    start ServiceLane

---
+++ src/main/java/com/hyunwoo/hackerrank/implementation/ServiceLane.java
@@ -0,0 +1,12 @@
+package com.hyunwoo.hackerrank.implementation;
+
+// https://www.hackerrank.com/challenges/service-lane/problem
+
+public class ServiceLane {
+
+    public int[] serviceLane(int n, int[][] cases) {
+
+        return null;
+    }
+
+}
---
+++ src/test/java/com/hyunwoo/hackerrank/implementation/ServiceLaneTest.java
@@ -0,0 +1,15 @@
+package com.hyunwoo.hackerrank.implementation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ServiceLaneTest {
+
+    @Test
+    public void serviceLane() {
+        ServiceLane serviceLane = new ServiceLane();
+
+
+    }
+}
