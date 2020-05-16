f1dfbb708705a015f4de31dbea3566f57cf6ad87
Hyunwoo Cho <barefoot1423@gmail.com>
2020-05-15 22:43:50 -0400

    start ManasaAndStonesTest

---
+++ src/test/java/com/hyunwoo/hackerrank/implementation/ManasaAndStonesTest.java
@@ -0,0 +1,13 @@
+package com.hyunwoo.hackerrank.implementation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ManasaAndStonesTest {
+
+    @Test
+    public void stones() {
+        ManasaAndStones manasaAndStones = new ManasaAndStones();
+    }
+}
