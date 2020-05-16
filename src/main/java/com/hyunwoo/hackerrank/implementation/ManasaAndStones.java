87ddf4b182e09ccfad6c70f9e5e4003b782d8f3d
Hyunwoo Cho <barefoot1423@gmail.com>
2020-05-15 22:42:57 -0400

    start ManasaAndStones

---
+++ src/main/java/com/hyunwoo/hackerrank/implementation/ManasaAndStones.java
@@ -0,0 +1,10 @@
+package com.hyunwoo.hackerrank.implementation;
+
+// https://www.hackerrank.com/challenges/manasa-and-stones/problem
+
+public class ManasaAndStones {
+    public int[] stones(int n, int a, int b) {
+
+        return null;
+    }
+}
