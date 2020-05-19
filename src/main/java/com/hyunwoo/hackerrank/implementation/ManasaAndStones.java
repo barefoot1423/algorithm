00b0e45694cbaee3d614295cbc3e99682d705346
Hyunwoo Cho <barefoot1423@gmail.com>
2020-05-18 23:48:04 -0400

    done ManasaAndStones

--- src/main/java/com/hyunwoo/hackerrank/implementation/ManasaAndStones.java
+++ src/main/java/com/hyunwoo/hackerrank/implementation/ManasaAndStones.java
@@ -2,10 +2,19 @@ package com.hyunwoo.hackerrank.implementation;
 
 // https://www.hackerrank.com/challenges/manasa-and-stones/problem
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ManasaAndStones {
     public int[] stones(int n, int a, int b) {
 
+        Set<Integer> retStone = new HashSet<>();
 
-        return null;
+        for (int i = 0; i < n; i++) {
+            int sum = (i * a) + ((n - 1 - i) * b);
+            retStone.add(sum);
+        }
+
+        return retStone.stream().mapToInt(Number::intValue).sorted().toArray();
     }
 }
--- src/test/java/com/hyunwoo/hackerrank/implementation/ManasaAndStonesTest.java
+++ src/test/java/com/hyunwoo/hackerrank/implementation/ManasaAndStonesTest.java
@@ -9,5 +9,8 @@ public class ManasaAndStonesTest {
     @Test
     public void stones() {
         ManasaAndStones manasaAndStones = new ManasaAndStones();
+
+        assertArrayEquals(new int[] {2, 3, 4}, manasaAndStones.stones(3, 1, 2));
+        assertArrayEquals(new int[] {30, 120, 210, 300}, manasaAndStones.stones(4, 10, 100));
     }
 }
