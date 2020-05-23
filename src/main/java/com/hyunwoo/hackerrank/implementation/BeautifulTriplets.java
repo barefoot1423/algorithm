70353b7462fba8544ef9f9b38a1891901c959d9f
Hyunwoo Cho <barefoot1423@gmail.com>
2020-05-23 13:31:55 -0400

    done BeautifulTriplets

--- src/main/java/com/hyunwoo/hackerrank/implementation/BeautifulTriplets.java
+++ src/main/java/com/hyunwoo/hackerrank/implementation/BeautifulTriplets.java
@@ -2,8 +2,22 @@ package com.hyunwoo.hackerrank.implementation;
 
 // https://www.hackerrank.com/challenges/beautiful-triplets/problem
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class BeautifulTriplets {
     public int beautifulTriplets(int d, int[] arr) {
-        return 0;
+        int ret = 0;
+
+        List<Integer> integerList = Arrays.stream(arr).boxed().collect(Collectors.toList());
+
+        for (int num : integerList) {
+            if (integerList.contains(num + d) && integerList.contains(num + 2 * d)) {
+                ret++;
+            }
+        }
+
+        return ret;
     }
 }
