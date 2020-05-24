0ea4a44227356478101c23bf5bc82cd64f0610a4
Hyunwoo Cho <barefoot1423@gmail.com>
2020-05-24 17:47:28 -0400

    start ACMICPCTeam

---
+++ src/main/java/com/hyunwoo/hackerrank/implementation/ACMICPCTeam.java
@@ -0,0 +1,9 @@
+package com.hyunwoo.hackerrank.implementation;
+
+// https://www.hackerrank.com/challenges/acm-icpc-team/problem
+public class ACMICPCTeam {
+    public int[] acmTeam(String[] topic) {
+
+        return null;
+    }
+}
---
+++ src/test/java/com/hyunwoo/hackerrank/implementation/ACMICPCTeamTest.java
@@ -0,0 +1,13 @@
+package com.hyunwoo.hackerrank.implementation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ACMICPCTeamTest {
+
+    @Test
+    public void acmTeam() {
+        ACMICPCTeam acmicpcTeam = new ACMICPCTeam();
+    }
+}
