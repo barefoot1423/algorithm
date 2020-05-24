dcf7cee2875dbaa036e7c20810eb9bcefbe309d7
Hyunwoo Cho <barefoot1423@gmail.com>
2020-05-24 17:44:29 -0400

    done TaumAndBday

--- src/main/java/com/hyunwoo/hackerrank/implementation/TaumAndBday.java
+++ src/main/java/com/hyunwoo/hackerrank/implementation/TaumAndBday.java
@@ -2,9 +2,18 @@ package com.hyunwoo.hackerrank.implementation;
 
 public class TaumAndBday {
     public long taumBday(int b, int w, int bc, int wc, int z) {
-        // Write your code here
 
+        int minPrice = Math.min(bc, wc);
+        int maxPrice = Math.max(bc, wc);
 
-        return 0L;
+        if (minPrice + z < maxPrice) {
+            if (maxPrice == bc) {
+                bc = minPrice + z;
+            } else {
+                wc = minPrice + z;
+            }
+        }
+
+        return ((long) b * (long) bc) + ((long) w * (long) wc);
     }
 }
