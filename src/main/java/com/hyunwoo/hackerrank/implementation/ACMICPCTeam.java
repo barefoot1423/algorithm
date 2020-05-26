263131d436031b4b3bacf28176673c939df099c3
Hyunwoo Cho <barefoot1423@gmail.com>
2020-05-25 23:15:46 -0400

    done ACMICPCTeam (Not my solution)

--- src/main/java/com/hyunwoo/hackerrank/implementation/ACMICPCTeam.java
+++ src/main/java/com/hyunwoo/hackerrank/implementation/ACMICPCTeam.java
@@ -1,9 +1,39 @@
 package com.hyunwoo.hackerrank.implementation;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
 // https://www.hackerrank.com/challenges/acm-icpc-team/problem
 public class ACMICPCTeam {
     public int[] acmTeam(String[] topic) {
 
-        return null;
+        // This is not my solution, but it is worth to keep and think about how other people resolve this issue.
+        int n = topic.length;
+        BigInteger[] bi = new BigInteger[n];
+
+        for (int i = 0; i < n; i++) {
+            bi[i] = new BigInteger(topic[i], 2);
+        }
+
+        int maxTopic = 0;
+        int teamCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                BigInteger iuj = bi[i].or(bi[j]);
+                int bitCount = iuj.bitCount();
+                if (bitCount > maxTopic) {
+                    maxTopic = bitCount;
+                    teamCount = 1;
+                } else if (bitCount == maxTopic) {
+                    teamCount++;
+                }
+            }
+        }
+
+        int result[] = {maxTopic, teamCount};
+        return result;
     }
+
 }
--- src/test/java/com/hyunwoo/hackerrank/implementation/ACMICPCTeamTest.java
+++ src/test/java/com/hyunwoo/hackerrank/implementation/ACMICPCTeamTest.java
@@ -9,5 +9,10 @@ public class ACMICPCTeamTest {
     @Test
     public void acmTeam() {
         ACMICPCTeam acmicpcTeam = new ACMICPCTeam();
+
+        String[] strings = new String[] {"10101", "11100", "11010", "00101"};
+
+        assertArrayEquals(new int[]{5, 2}, acmicpcTeam.acmTeam(strings));
     }
+
 }
