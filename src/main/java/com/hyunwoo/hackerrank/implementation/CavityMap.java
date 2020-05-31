8fff3217300f9370eb896e859e19ba4a9e224ced
Hyunwoo Cho <barefoot1423@gmail.com>
2020-05-30 22:50:08 -0400

    start CavityMap

---
+++ src/main/java/com/hyunwoo/hackerrank/implementation/CavityMap.java
@@ -0,0 +1,9 @@
+package com.hyunwoo.hackerrank.implementation;
+
+// https://www.hackerrank.com/challenges/cavity-map/problem
+
+public class CavityMap {
+    public String[] cavityMap(String[] grid) {
+        return null;
+    }
+}
---
+++ src/test/java/com/hyunwoo/hackerrank/implementation/CavityMapTest.java
@@ -0,0 +1,15 @@
+package com.hyunwoo.hackerrank.implementation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CavityMapTest {
+
+    @Test
+    public void cavityMap() {
+        CavityMap cavityMap = new CavityMap();
+        assertArrayEquals(new String[]{"1112", "1X12", "18X2", "1234"}, cavityMap.cavityMap(new String[]{"1112", "1912", "1892", "1234"}));
+    }
+
+}
