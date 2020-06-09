ed83dd57afcc8a4fdf9d62f966ae8d6739efcc64
Hyunwoo Cho <barefoot1423@gmail.com>
2020-06-08 23:59:01 -0400

    start StrangeCounter

---
+++ src/main/java/com/hyunwoo/hackerrank/implementation/StrangeCounter.java
@@ -0,0 +1,12 @@
+package com.hyunwoo.hackerrank.implementation;
+
+// https://www.hackerrank.com/challenges/strange-code/problem
+
+public class StrangeCounter {
+
+    public long strangeCounter(long t) {
+
+        return 0L;
+    }
+
+}
