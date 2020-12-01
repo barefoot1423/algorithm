cc3cc02dbec88180f6849fb94aab42cf04e83815
Hyunwoo Cho <barefoot1423@gmail.com>
2020-11-30 23:15:09 -0500

    Done cavityMap

--- src/main/java/com/hyunwoo/hackerrank/implementation/CavityMap.java
+++ src/main/java/com/hyunwoo/hackerrank/implementation/CavityMap.java
@@ -3,7 +3,21 @@ package com.hyunwoo.hackerrank.implementation;
 // https://www.hackerrank.com/challenges/cavity-map/problem
 
 public class CavityMap {
-    public String[] cavityMap(String[] grid) {
-        return null;
+
+    static String[] cavityMap(String[] grid) {
+        for (int i = 1; i < grid.length - 1; i++) {
+            for (int j = 1; j < grid[i].length() - 1; j++) {
+                if (grid[i].charAt(j) > grid[i].charAt(j + 1) &&
+                    grid[i].charAt(j) > grid[i].charAt(j - 1) &&
+                    grid[i].charAt(j) > grid[i + 1].charAt(j) &&
+                    grid[i].charAt(j) > grid[i - 1].charAt(j)) {
+                    grid[i] = grid[i].substring(0, j) + "X" +
+                        grid[i].substring(j + 1);
+                }
+            }
+        }
+
+        return grid;
     }
+
 }
