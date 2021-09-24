75978b1a416968c0a96d418f114f6c450d5f7c2c
Hyunwoo Cho <barefoot1423@gmail.com>
2021-09-23 22:41:32 -0400

    practice stringEquals method

---
+++ src/main/java/com/hyunwoo/interview/practice/StringEquals.java
@@ -0,0 +1,16 @@
+package com.hyunwoo.interview.practice;
+
+public class StringEquals {
+
+    public static void main(String[] args) {
+
+        String s1 = "Hello";
+        String s2 = new String(s1);
+        String s3 = "Hello";
+
+        System.out.println("s1(" + s1 + ") equals " + "s2(" + s2 + ")--> " + s1.equals(s2)); //true
+        System.out.println("s1(" + s1 + ") == " + "s2(" + s2 + ") --> " + (s1 == s2)); //false
+        System.out.println("s1(" + s1 + ") == " + "s3(" + s3 + ") --> " + (s1 == s3)); //true
+    }
+
+}
