cfc778e88e523cc6d246d77e4731ef73ad5336d6
Hyunwoo Cho <barefoot1423@gmail.com>
2023-01-26 21:43:22 -0500

    commit MergeTwoSortedList recursion solution

---
+++ src/test/java/com/hyunwoo/leetCode/mergeTwoSortedlist/MergeTwoSortedListTest.java
@@ -0,0 +1,14 @@
+package com.hyunwoo.leetCode.mergeTwoSortedlist;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MergeTwoSortedListTest {
+
+    @Test
+    void mergeTwoLists() {
+        var mergeTwoSortedlist = new MergeTwoSortedList();
+    }
+
+}
