066cdc594281dc8f75d412c0746393d350c19e07
Hyunwoo Cho <barefoot1423@gmail.com>
2023-08-19 16:27:59 -0400

    [026] my hashset solution

--- src/main/java/com/hyunwoo/leetCode/RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/com/hyunwoo/leetCode/RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.java
@@ -1,8 +1,22 @@
 //https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 package com.hyunwoo.leetCode.RemoveDuplicatesFromSortedArray;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class RemoveDuplicatesFromSortedArray {
+
     public int removeDuplicates(int[] nums) {
-        return 0;
+
+        Set set = new HashSet<Integer>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.contains(nums[i])) {
+                set.add(nums[i]);
+            }
+        }
+
+        return set.size();
     }
+
 }
