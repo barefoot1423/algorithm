ba66289603d27aa8ba531dd1439c44dab68b942c
Hyunwoo Cho <barefoot1423@gmail.com>
2023-08-19 18:12:14 -0400

    ˆ

--- src/main/java/com/hyunwoo/leetCode/RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/com/hyunwoo/leetCode/RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.java
@@ -8,11 +8,13 @@ public class RemoveDuplicatesFromSortedArray {
 
     public int removeDuplicates(int[] nums) {
 
-        Set set = new HashSet<Integer>();
+        Set<Integer> set = new HashSet<>();
+        int j = 0;
 
         for (int i = 0; i < nums.length; i++) {
             if (!set.contains(nums[i])) {
                 set.add(nums[i]);
+                nums[j++] = nums[i];
             }
         }
 
