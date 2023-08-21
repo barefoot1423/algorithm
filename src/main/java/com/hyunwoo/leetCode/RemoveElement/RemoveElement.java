22e5bf7cfc216c7a62239e4e741a75f97828f7a3
Hyunwoo Cho <barefoot1423@gmail.com>
2023-08-20 22:49:35 -0400

    commit my solution for 027 problem

--- src/main/java/com/hyunwoo/leetCode/RemoveElement/RemoveElement.java
+++ src/main/java/com/hyunwoo/leetCode/RemoveElement/RemoveElement.java
@@ -2,8 +2,22 @@
 
 package com.hyunwoo.leetCode.RemoveElement;
 
+import java.util.Arrays;
+
 public class RemoveElement {
     public int removeElement(int[] nums, int val) {
-        return 0;
+
+        int ret = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+            } else {
+                nums[ret] = nums[i];
+                ret++;
+            }
+        }
+
+        System.out.println(Arrays.toString(nums));
+
+        return ret;
     }
 }
