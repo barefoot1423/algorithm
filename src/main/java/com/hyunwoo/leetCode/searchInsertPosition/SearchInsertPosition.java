a61ce302cea5c94f6f3b65b7767117f142f65a6b
Hyunwoo Cho <barefoot1423@gmail.com>
2023-08-24 23:52:21 -0400

    start working on 035

---
+++ src/main/java/com/hyunwoo/leetCode/searchInsertPosition/SearchInsertPosition.java
@@ -0,0 +1,8 @@
+//https://leetcode.com/problems/search-insert-position/
+package com.hyunwoo.leetCode.searchInsertPosition;
+
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/hyunwoo/leetCode/searchInsertPosition/SearchInsertPositionTest.java
@@ -0,0 +1,13 @@
+package com.hyunwoo.leetCode.searchInsertPosition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SearchInsertPositionTest {
+
+    @Test
+    void searchInsert() {
+    }
+
+}
