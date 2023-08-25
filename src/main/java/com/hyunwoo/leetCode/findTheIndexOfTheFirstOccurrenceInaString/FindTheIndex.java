05e0e41e36f26bc4e97eb323019fe201d226c761
Hyunwoo Cho <barefoot1423@gmail.com>
2023-08-24 23:50:34 -0400

    commit another solution for 028

--- src/main/java/com/hyunwoo/leetCode/findTheIndexOfTheFirstOccurrenceInaString/FindTheIndex.java
+++ src/main/java/com/hyunwoo/leetCode/findTheIndexOfTheFirstOccurrenceInaString/FindTheIndex.java
@@ -4,37 +4,10 @@ package com.hyunwoo.leetCode.findTheIndexOfTheFirstOccurrenceInaString;
 public class FindTheIndex {
 
     public int strStr(String haystack, String needle) {
-
-        char[] haystackCharArray = haystack.toCharArray();
-        char[] needleCharArray = needle.toCharArray();
-        int ret = -1;
-        if (haystackCharArray.length < needleCharArray.length) {
+        if (haystack.contains(needle)) {
+            return haystack.indexOf(needle);
+        } else {
             return -1;
         }
-
-        for (int i = 0; i < haystackCharArray.length; ) {
-            for (int j = 0; j < needleCharArray.length && i < haystackCharArray.length; j++) {
-                if (haystackCharArray[i] == needleCharArray[j]) {
-                    if (ret == -1) {
-                        ret = i;
-                    }
-                    i++;
-                } else {
-                    if (ret != -1) {
-                        i = ret + 1;
-                    } else {
-                        i++;
-                    }
-                    ret = -1;
-                    break;
-                }
-                if (j == needleCharArray.length - 1) {
-                    return ret;
-                }
-            }
-        }
-
-        return -1;
     }
-
 }
