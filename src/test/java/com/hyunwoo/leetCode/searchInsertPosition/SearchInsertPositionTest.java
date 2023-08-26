38751ac9443b5b601fd5ad8ad699303a83aa3d85
Hyunwoo Cho <barefoot1423@gmail.com>
2023-08-25 21:40:39 -0400

    add test casesˆ

--- src/test/java/com/hyunwoo/leetCode/searchInsertPosition/SearchInsertPositionTest.java
+++ src/test/java/com/hyunwoo/leetCode/searchInsertPosition/SearchInsertPositionTest.java
@@ -8,6 +8,10 @@ class SearchInsertPositionTest {
 
     @Test
     void searchInsert() {
+        SearchInsertPosition searchInsertPosition = new SearchInsertPosition();
+        assertEquals(2, searchInsertPosition.searchInsert(new int[] {1, 3, 5, 6}, 5));
+        assertEquals(1, searchInsertPosition.searchInsert(new int[] {1, 3, 5, 6}, 2));
+        assertEquals(4, searchInsertPosition.searchInsert(new int[] {1, 3, 5, 6}, 7));
     }
 
 }
