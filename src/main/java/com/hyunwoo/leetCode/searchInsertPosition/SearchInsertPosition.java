ecbe7a46af777db342f31cb7742a82fa110fa2c5
Hyunwoo Cho <barefoot1423@gmail.com>
2023-08-30 22:30:03 -0400

    solution plan

--- src/main/java/com/hyunwoo/leetCode/searchInsertPosition/SearchInsertPosition.java
+++ src/main/java/com/hyunwoo/leetCode/searchInsertPosition/SearchInsertPosition.java
@@ -3,6 +3,9 @@ package com.hyunwoo.leetCode.searchInsertPosition;
 
 public class SearchInsertPosition {
     public int searchInsert(int[] nums, int target) {
+        // implement quick sort O(nlogn)
+        // find a position
+        // return the position
         return 0;
     }
 }
