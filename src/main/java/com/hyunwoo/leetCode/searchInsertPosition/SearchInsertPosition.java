dc91237a00cd302b14dce1600357de1e1e206797
Hyunwoo Cho <barefoot1423@gmail.com>
2023-09-05 23:36:40 -0400

    commit my solution 035

--- src/main/java/com/hyunwoo/leetCode/searchInsertPosition/SearchInsertPosition.java
+++ src/main/java/com/hyunwoo/leetCode/searchInsertPosition/SearchInsertPosition.java
@@ -3,10 +3,42 @@ package com.hyunwoo.leetCode.searchInsertPosition;
 
 public class SearchInsertPosition {
     public int searchInsert(int[] nums, int target) {
-        // implement quick sort O(nlogn)
-        // find a position
-        // return the positions
+        int end = nums.length - 1;
+        int start = 0;
+
+        while (start <= end) {
+            int mid = (end - start) / 2 + start;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                int foundIndex = checkIndex(nums, target, mid);
+                if (foundIndex != -1) {
+                    return foundIndex;
+                }
+                end = mid - 1;
+            } else {
+                int foundIndex = checkIndex(nums, target, mid);
+                if (foundIndex != -1) {
+                    return foundIndex;
+                }
+                start = mid + 1;
+            }
+        }
+
+        if (nums[nums.length - 1] < target) {
+            return nums.length;
+        }
 
         return 0;
     }
+
+    private static int checkIndex(int[] nums, int target, int mid) {
+        if (mid - 1 >= 0 && mid + 1 <= nums.length) {
+            if (nums[mid -1] < target && nums[mid] > target) {
+                return mid ;
+            }
+        }
+        return -1;
+    }
+
 }
--- src/test/java/com/hyunwoo/leetCode/searchInsertPosition/SearchInsertPositionTest.java
+++ src/test/java/com/hyunwoo/leetCode/searchInsertPosition/SearchInsertPositionTest.java
@@ -12,6 +12,7 @@ class SearchInsertPositionTest {
         assertEquals(2, searchInsertPosition.searchInsert(new int[] {1, 3, 5, 6}, 5));
         assertEquals(1, searchInsertPosition.searchInsert(new int[] {1, 3, 5, 6}, 2));
         assertEquals(4, searchInsertPosition.searchInsert(new int[] {1, 3, 5, 6}, 7));
+        assertEquals(1, searchInsertPosition.searchInsert(new int[] {1}, 2));
     }
 
 }
