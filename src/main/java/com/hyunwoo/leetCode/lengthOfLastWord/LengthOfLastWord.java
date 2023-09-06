d155cddddc38d9633e4606c35102499ad5014b70
Hyunwoo Cho <barefoot1423@gmail.com>
2023-09-05 23:40:59 -0400

    start leecode 058

---
+++ src/main/java/com/hyunwoo/leetCode/lengthOfLastWord/LengthOfLastWord.java
@@ -0,0 +1,8 @@
+//https://leetcode.com/problems/length-of-last-word/
+package com.hyunwoo.leetCode.lengthOfLastWord;
+
+public class LengthOfLastWord {
+    public int lengthOfLastWord(String s) {
+        return 0;
+    }
+}
