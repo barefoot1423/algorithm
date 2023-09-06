23970abf6b9a55609807a7d539525c85b603a81c
Hyunwoo Cho <barefoot1423@gmail.com>
2023-09-05 23:38:37 -0400

    commit my best solution 035

--- src/main/java/com/hyunwoo/leetCode/searchInsertPosition/SearchInsertPosition.java
+++ src/main/java/com/hyunwoo/leetCode/searchInsertPosition/SearchInsertPosition.java
@@ -11,34 +11,13 @@ public class SearchInsertPosition {
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] > target) {
-                int foundIndex = checkIndex(nums, target, mid);
-                if (foundIndex != -1) {
-                    return foundIndex;
-                }
                 end = mid - 1;
             } else {
-                int foundIndex = checkIndex(nums, target, mid);
-                if (foundIndex != -1) {
-                    return foundIndex;
-                }
                 start = mid + 1;
             }
         }
 
-        if (nums[nums.length - 1] < target) {
-            return nums.length;
-        }
-
-        return 0;
-    }
-
-    private static int checkIndex(int[] nums, int target, int mid) {
-        if (mid - 1 >= 0 && mid + 1 <= nums.length) {
-            if (nums[mid -1] < target && nums[mid] > target) {
-                return mid ;
-            }
-        }
-        return -1;
+        return start;
     }
 
 }
