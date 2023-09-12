8463e0a1fec4affdc0f0c7a9274eb3f7965a1473
Hyunwoo Cho <barefoot1423@gmail.com>
2023-09-12 00:01:10 -0400

    This is not my solution, but it is worth to be my reference

--- src/main/java/com/hyunwoo/leetCode/plusOne/PlusOne.java
+++ src/main/java/com/hyunwoo/leetCode/plusOne/PlusOne.java
@@ -1,20 +1,24 @@
 //https://leetcode.com/problems/plus-one/
 package com.hyunwoo.leetCode.plusOne;
 
-import java.util.Arrays;
-
 public class PlusOne {
 
     public int[] plusOne(int[] digits) {
-        StringBuilder num = new StringBuilder();
-        Arrays.stream(digits).forEach(s -> num.append(s));
-        String returnNum = String.valueOf(Long.parseLong(num.toString()) + 1);
-        String[] stringReturn = returnNum.split("");
-        System.out.println(Arrays.toString(stringReturn));
-        int[] array = Arrays.stream(stringReturn).mapToInt(s -> Integer.parseInt(s)).toArray();
 
-        System.out.println(Arrays.toString(array));
-        return array;
+        int len = digits.length;
+
+        for (int i = len - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] retDigits = new int[len + 1];
+        retDigits[0] = 1;
+
+        return retDigits;
     }
 
 }
