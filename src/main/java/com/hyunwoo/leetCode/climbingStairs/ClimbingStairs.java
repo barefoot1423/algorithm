9f68b568484fe2fe7d0942c24b64bc67a465f26b
Hyunwoo Cho <barefoot1423@gmail.com>
2023-09-14 23:32:45 -0400

    Solution from Leetcode

--- src/main/java/com/hyunwoo/leetCode/climbingStairs/ClimbingStairs.java
+++ src/main/java/com/hyunwoo/leetCode/climbingStairs/ClimbingStairs.java
@@ -1,10 +1,25 @@
 //https://leetcode.com/problems/climbing-stairs/
+//https://leetcode.com/problems/climbing-stairs/solutions/3708750/4-method-s-beat-s-100-c-java-python-beginner-friendly/
 package com.hyunwoo.leetCode.climbingStairs;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ClimbingStairs {
 
     public int climbStairs(int n) {
-        return 0;
+        Map<Integer, Integer> memo = new HashMap<>();
+        return climbStairs(n, memo);
+    }
+
+    private int climbStairs(int n, Map<Integer, Integer> memo) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        if (!memo.containsKey(n)) {
+            memo.put(n, climbStairs(n - 1, memo) + climbStairs(n - 2, memo));
+        }
+        return memo.get(n);
     }
 
 }
