c961c1791da1a9021194d8b9458b023c8a81b0e2
Hyunwoo Cho <barefoot1423@gmail.com>
2023-09-21 23:28:55 -0400

    commit fibonacci

---
+++ src/main/java/com/hyunwoo/javaAlgorithmBook/fibonacci/Fibonacci.java
@@ -0,0 +1,19 @@
+package com.hyunwoo.javaAlgorithmBook.fibonacci;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        int n = 6;
+        System.out.println(fibonacci(n));
+    }
+
+    private static long fibonacci(int n) {
+
+        if (n <= 1) {
+            return n;
+        }
+
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+}
