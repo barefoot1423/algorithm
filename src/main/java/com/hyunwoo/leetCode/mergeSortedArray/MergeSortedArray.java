7b7b1120e29881f558cc941ebd1f6c0bde6779f0
Hyunwoo Cho <barefoot1423@gmail.com>
2023-09-28 22:08:26 -0400

    start leetcode 088

---
+++ src/main/java/com/hyunwoo/leetCode/mergeSortedArray/MergeSortedArray.java
@@ -0,0 +1,11 @@
+package com.hyunwoo.leetCode.mergeSortedArray;
+
+public class MergeSortedArray {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int[] ret = new int[m + n];
+
+    }
+
+}
--- src/main/java/com/hyunwoo/leetCode/removeDuplicatesFromSortedlist/RemoveDuplicatesFromSortedList.java
+++ src/main/java/com/hyunwoo/leetCode/removeDuplicatesFromSortedlist/RemoveDuplicatesFromSortedList.java
@@ -6,11 +6,11 @@ public class RemoveDuplicatesFromSortedList {
     public ListNode deleteDuplicates(ListNode head) {
 
         ListNode list = head;
-        while (list.next != null) {
+
+        while (list != null) {
             if (list.next == null) {
                 break;
             }
-
             if (list.val == list.next.val) {
                 list.next = list.next.next;
             } else {
