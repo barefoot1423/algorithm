7cdf39b1d90eeefaa541bc68c609da8df428713c
Hyunwoo Cho <barefoot1423@gmail.com>
2023-10-03 19:30:38 -0400

    commit LinkedListNodeˆ

---
+++ src/main/java/com/hyunwoo/javaAlgorithmBook/linkedList/LinkedListNode.java
@@ -0,0 +1,19 @@
+package com.hyunwoo.javaAlgorithmBook.linkedList;
+
+import java.util.Optional;
+
+public class LinkedListNode<V> {
+
+    private V value;
+    private LinkedListNode<V> next;
+
+    public LinkedListNode(V value, LinkedListNode<V> next) {
+        this.value = value;
+        this.next = next;
+    }
+
+    public Optional<LinkedListNode<V>> getNext() {
+        return Optional.ofNullable(next);
+    }
+
+}
