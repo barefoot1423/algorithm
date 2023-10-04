a0aaf82f0177dc3f2baf49021a252ac34307f950
Hyunwoo Cho <barefoot1423@gmail.com>
2023-10-03 20:05:07 -0400

    commit ˆDoubleLinkedListNode

--- src/main/java/com/hyunwoo/javaAlgorithmBook/linkedList/DoubleLinkedListNode.java
+++ src/main/java/com/hyunwoo/javaAlgorithmBook/linkedList/DoubleLinkedListNode.java
@@ -1,15 +1,45 @@
 package com.hyunwoo.javaAlgorithmBook.linkedList;
 
-public class DoubleLinkedListNode<T> {
+import java.util.Optional;
 
-    private T value;
-    private DoubleLinkedListNode<T> next;
-    private DoubleLinkedListNode<T> previous;
+public class DoubleLinkedListNode<V> {
 
-    public DoubleLinkedListNode(T value, DoubleLinkedListNode<T> next, DoubleLinkedListNode<T> previous) {
+    private V value;
+    private DoubleLinkedListNode<V> next;
+    private DoubleLinkedListNode<V> previous;
+
+    public DoubleLinkedListNode(V value, DoubleLinkedListNode<V> next, DoubleLinkedListNode<V> previous) {
         this.value = value;
         this.next = next;
         this.previous = previous;
+    }
+
+    public Optional<DoubleLinkedListNode<V>> getNext() {
+        return Optional.ofNullable(next);
+    }
+
+    public Optional<DoubleLinkedListNode<V>> getPrevious() {
+        return Optional.ofNullable(previous);
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public DoubleLinkedListNode setValue(V value) {
+        this.value = value;
+        return this;
+    }
+
+    public DoubleLinkedListNode setNext(DoubleLinkedListNode<V> next) {
+        this.next = next;
+        return this;
+    }
+
+
+    public DoubleLinkedListNode setPrevious(DoubleLinkedListNode<V> previous) {
+        this.previous = previous;
+        return this;
     }
 
 }
