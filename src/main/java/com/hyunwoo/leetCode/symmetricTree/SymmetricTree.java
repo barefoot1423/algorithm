f3897ef08c72e707accea56492b3297775aaeb2c
Hyunwoo Cho <barefoot1423@gmail.com>
2023-10-04 21:31:42 -0400

    start leetcode 101ˆ

---
+++ src/main/java/com/hyunwoo/leetCode/symmetricTree/SymmetricTree.java
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/symmetric-tree/
+package com.hyunwoo.leetCode.symmetricTree;
+
+public class SymmetricTree {
+
+    public boolean isSymmetric(TreeNode root) {
+
+        return false;
+
+    }
+
+}
