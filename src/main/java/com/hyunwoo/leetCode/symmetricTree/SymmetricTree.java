4961b7ea4e9f7a0742a4f939683b61cb38ff276a
Hyunwoo Cho <barefoot1423@gmail.com>
2023-10-05 20:25:49 -0400

    still working on leetcode 101

--- src/main/java/com/hyunwoo/leetCode/symmetricTree/SymmetricTree.java
+++ src/main/java/com/hyunwoo/leetCode/symmetricTree/SymmetricTree.java
@@ -1,12 +1,43 @@
 //https://leetcode.com/problems/symmetric-tree/
 package com.hyunwoo.leetCode.symmetricTree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SymmetricTree {
+
+    List<Integer> inorderList = new ArrayList<>();
 
     public boolean isSymmetric(TreeNode root) {
 
-        return false;
+        inorder(root);
 
+        int mid = inorderList.size() / 2;
+        int i = mid;
+        int j = mid + 1;
+        for (; i <= 0; ) {
+            for (; j <= inorderList.size(); ) {
+                if (inorderList.get(i) != inorderList.get(j)) {
+                    return false;
+                }
+            }
+            i--;
+            j++;
+        }
+
+        return true;
+
+    }
+
+    private void inorder(TreeNode root) {
+
+        if (root == null ) {
+            return;
+        }
+
+        inorder(root.left);
+        inorderList.add(root.val);
+        inorder(root.right);
     }
 
 }
