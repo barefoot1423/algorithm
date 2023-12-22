6134f1a0154a45af91a73a032161c534446aabbf
Hyunwoo Cho <barefoot1423@gmail.com>
2023-12-21 23:30:28 -0500

    start Leecode 169

---
+++ src/main/java/com/hyunwoo/leetCode/majorityElements.java
@@ -0,0 +1,8 @@
+//https://leetcode.com/problems/majority-element
+package com.hyunwoo.leetCode;
+
+public class majorityElements {
+    public int majorityElement(int[] nums) {
+        return 0;
+    }
+}
