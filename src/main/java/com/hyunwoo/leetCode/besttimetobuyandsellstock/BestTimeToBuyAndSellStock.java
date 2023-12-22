12d928bcacef90b7fca0291a0213cd37be7c71f8
Hyunwoo Cho <barefoot1423@gmail.com>
2023-12-22 14:41:56 -0500

    start Leetcode 121

---
+++ src/main/java/com/hyunwoo/leetCode/besttimetobuyandsellstock/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+package com.hyunwoo.leetCode.besttimetobuyandsellstock;
+
+public class BestTimeToBuyAndSellStock {
+
+    public int maxProfit(int[] prices) {
+
+        return 0;
+    }
+
+}
