3b6e03f7f38660ee8c53e9f5ab40fa802b5e2185
Hyunwoo Cho <barefoot1423@gmail.com>
2023-12-24 19:15:49 -0500

    Done Leetcode 88

--- src/main/java/com/hyunwoo/leetCode/interview150/p88/MergeSortedArray.java
+++ src/main/java/com/hyunwoo/leetCode/interview150/p88/MergeSortedArray.java
@@ -21,4 +21,21 @@ public class MergeSortedArray {
         Arrays.stream(nums1).forEach(s -> System.out.println(s));
     }
 
+    public void improvedMerge(int[] nums1, int m, int[] nums2, int n) {
+        int[] copyNums1 = Arrays.copyOf(nums1, m);
+
+        int p1 = 0;
+        int p2 = 0;
+
+        for (int i = 0; i < m + n; i++) {
+            if (p2 >= n || (p1 < m && copyNums1[p1] < nums2[p2])) {
+                nums1[i] = copyNums1[p1++];
+            } else {
+                nums1[i] = nums2[p2++];
+            }
+        }
+
+        Arrays.stream(nums1).forEach(s -> System.out.println(s));
+    }
+
 }
--- src/test/java/com/hyunwoo/leetCode/interview150/p88/MergeSortedArrayTest.java
+++ src/test/java/com/hyunwoo/leetCode/interview150/p88/MergeSortedArrayTest.java
@@ -10,7 +10,8 @@ class MergeSortedArrayTest {
     void merge() {
         MergeSortedArray mergeSortedArray = new MergeSortedArray();
 //        mergeSortedArray.merge(new int[] {1, 2, 3, 0, 0, 0}, 3, new int[] {2, 5, 6}, 3);
-        mergeSortedArray.merge(new int[] {-1, 0, 0, 3, 3, 3, 0, 0, 0}, 6, new int[] {1, 2, 2}, 3);
+//        mergeSortedArray.merge(new int[] {-1, 0, 0, 3, 3, 3, 0, 0, 0}, 6, new int[] {1, 2, 2}, 3);
+        mergeSortedArray.improvedMerge(new int[] {-1, 0, 0, 3, 3, 3, 0, 0, 0}, 6, new int[] {1, 2, 2}, 3);
 //        assertArrayEquals(new int[] {1, 2, 2, 3, 5, 6}, mergeSortedArray.merge(new int[] {1, 2, 3, 0, 0, 0}, 3, new int[] {2, 5, 6}, 3));
     }
 
