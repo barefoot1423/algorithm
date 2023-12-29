247d55a8e583aaa501d3d114fdf162f771bf56bb
Hyunwoo Cho <barefoot1423@gmail.com>
2023-12-28 22:40:57 -0500

    start leetcode p13

---
+++ src/main/java/com/hyunwoo/leetCode/interview150/p13/RomanToInteger.java
@@ -0,0 +1,20 @@
+package com.hyunwoo.leetCode.interview150.p13;
+
+import java.util.Arrays;
+
+public class RomanToInteger {
+
+    public int romanToInt(String s) {
+
+        int ret = 0;
+        s.chars().forEach(a -> {
+            if (a == 'V') {
+//                ret += 1;
+            }
+        });
+
+
+        return 0;
+    }
+
+}
