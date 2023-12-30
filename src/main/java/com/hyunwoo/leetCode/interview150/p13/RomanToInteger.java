18ce30b1de8d2bb8f0246d2a7b028defc4292645
Hyunwoo Cho <barefoot1423@gmail.com>
2023-12-30 16:10:45 -0500

    done leetcode p13

--- src/main/java/com/hyunwoo/leetCode/interview150/p13/RomanToInteger.java
+++ src/main/java/com/hyunwoo/leetCode/interview150/p13/RomanToInteger.java
@@ -1,20 +1,35 @@
 package com.hyunwoo.leetCode.interview150.p13;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RomanToInteger {
 
     public int romanToInt(String s) {
 
+        Map<Character, Integer> m = new HashMap<>();
+
+        m.put('I', 1);
+        m.put('V', 5);
+        m.put('X', 10);
+        m.put('L', 50);
+        m.put('C', 100);
+        m.put('D', 500);
+        m.put('M', 1000);
+
         int ret = 0;
-        s.chars().forEach(a -> {
-            if (a == 'V') {
-                ret += 1;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1 && m.get(s.charAt(i)) < m.get(s.charAt(i + 1))) {
+                ret -= m.get(s.charAt(i));
+            } else {
+                ret += m.get(s.charAt(i));
             }
-        });
+        }
 
+        return ret;
 
-        return 0;
     }
 
 }
---
+++ src/test/java/com/hyunwoo/leetCode/interview150/p13/RomanToIntegerTest.java
@@ -0,0 +1,19 @@
+package com.hyunwoo.leetCode.interview150.p13;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RomanToIntegerTest {
+
+    @Test
+    void romanToInt() {
+        var romanToInterger = new RomanToInteger();
+
+        assertEquals(3, romanToInterger.romanToInt("III"));
+        assertEquals(58, romanToInterger.romanToInt("LVIII"));
+        assertEquals(1994, romanToInterger.romanToInt("MCMXCIV"));
+
+    }
+
+}
