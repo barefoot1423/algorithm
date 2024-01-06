29d914df312861d7d49c4894d3d94810beed3b84
Hyunwoo Cho <barefoot1423@gmail.com>
2024-01-06 10:24:38 -0500

    start leetcode p209

---
+++ src/main/java/com/hyunwoo/leetCode/interview150/p209/WordPattern.java
@@ -0,0 +1,8 @@
+package com.hyunwoo.leetCode.interview150.p209;
+
+public class WordPattern {
+    public boolean wordPattern(String pattern, String s) {
+
+        return false;
+    }
+}
---
+++ src/test/java/com/hyunwoo/leetCode/interview150/p209/WordPatternTest.java
@@ -0,0 +1,18 @@
+package com.hyunwoo.leetCode.interview150.p209;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WordPatternTest {
+
+    @Test
+    void wordPattern() {
+        WordPattern wordPattern = new WordPattern();
+
+        assertEquals(true, wordPattern.wordPattern("abba", "dog cat cat dog"));
+        assertEquals(true, wordPattern.wordPattern("abba", "dog cat cat fish"));
+        assertEquals(true, wordPattern.wordPattern("aaaa", "dog cat cat dog"));
+    }
+
+}
