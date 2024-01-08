c84a15c9a579add0ec9e46e5618ab8191451820d
Hyunwoo Cho <barefoot1423@gmail.com>
2024-01-07 21:44:44 -0500

    start leetcode p202

---
+++ src/main/java/com/hyunwoo/leetCode/interview150/p202/HappyNumber.java
@@ -0,0 +1,7 @@
+package com.hyunwoo.leetCode.interview150.p202;
+
+public class HappyNumber {
+    public boolean isHappy(int n) {
+        return false;
+    }
+}
---
+++ src/test/java/com/hyunwoo/leetCode/interview150/p202/HappyNumberTest.java
@@ -0,0 +1,15 @@
+package com.hyunwoo.leetCode.interview150.p202;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HappyNumberTest {
+
+    @Test
+    void isHappy() {
+        HappyNumber happyNumber = new HappyNumber();
+        assertEquals(true, happyNumber.isHappy(19));
+    }
+
+}
