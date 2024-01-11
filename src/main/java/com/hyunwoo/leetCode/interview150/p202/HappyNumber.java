465242354fde9deca97fa2ed772fdf5a6ecd4e00
Hyunwoo Cho <barefoot1423@gmail.com>
2024-01-10 23:40:22 -0500

    done leetcode p202

--- src/main/java/com/hyunwoo/leetCode/interview150/p202/HappyNumber.java
+++ src/main/java/com/hyunwoo/leetCode/interview150/p202/HappyNumber.java
@@ -1,32 +1,37 @@
 package com.hyunwoo.leetCode.interview150.p202;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class HappyNumber {
     public boolean isHappy(int n) {
 
-        boolean ret = true;
         int sum = 0;
 
-        if (n == 1 ) {
-            return true;
-        }
-
         List<Integer> collect = String.valueOf(n).chars().map(Character::getNumericValue).boxed().collect(Collectors.toList());
+        Set<Integer> valueSet = new HashSet<>();
 
-        while (collect.stream().mapToInt(Integer::intValue).sum() != 1) {
+        while (!valueSet.contains(sum)) {
 
             for (Integer num : collect) {
                 sum += Math.pow(num, 2);
             }
-            System.out.println(sum);
-
 
             collect = String.valueOf(sum).chars().map(Character::getNumericValue).boxed().collect(Collectors.toList());
+            if (collect.stream().mapToInt(Integer::intValue).sum() == 1) {
+                return true;
+            } else {
+                if (!valueSet.contains(sum)) {
+                    valueSet.add(sum);
+                } else {
+                    return false;
+                }
+            }
             sum = 0;
         }
 
-        return ret;
+        return false;
     }
 }
--- src/test/java/com/hyunwoo/leetCode/interview150/p202/HappyNumberTest.java
+++ src/test/java/com/hyunwoo/leetCode/interview150/p202/HappyNumberTest.java
@@ -9,8 +9,8 @@ class HappyNumberTest {
     @Test
     void isHappy() {
         HappyNumber happyNumber = new HappyNumber();
-        assertEquals(true, happyNumber.isHappy(19));
-        assertEquals(false, happyNumber.isHappy(2));
+//        assertEquals(true, happyNumber.isHappy(19));
+//        assertEquals(false, happyNumber.isHappy(2));
         assertEquals(false, happyNumber.isHappy(7));
     }
 
