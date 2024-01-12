59761f078f0a6070c901d6bb12f9f8222f70df40
Hyunwoo Cho <barefoot1423@gmail.com>
2024-01-11 23:17:46 -0500

    done leetcode p219

--- src/main/java/com/hyunwoo/leetCode/interview150/p219/ContainsDuplicate.java
+++ src/main/java/com/hyunwoo/leetCode/interview150/p219/ContainsDuplicate.java
@@ -1,8 +1,39 @@
 package com.hyunwoo.leetCode.interview150.p219;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class ContainsDuplicate {
+
     public boolean containsNearbyDuplicate(int[] nums, int k) {
 
+        Map<Integer, List<Integer>> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+            if (!map.containsKey(n)) {
+                map.put(n, new ArrayList<>(5));
+                map.get(n).add(i);
+            } else {
+                map.get(n).add((i));
+            }
+        }
+
+        List<Map.Entry<Integer, List<Integer>>> collect = map.entrySet().stream().filter(m -> m.getValue().size() > 1).collect(Collectors.toList());
+
+        for (int i = 0; i < collect.size(); i++) {
+            List<Integer> value = collect.get(i).getValue();
+            for (int j = 0; j < value.size() - 1; j++) {
+                if (Math.abs(value.get(j) - value.get(j + 1)) <= k) {
+                    return true;
+                }
+            }
+
+        }
         return false;
     }
+
 }
