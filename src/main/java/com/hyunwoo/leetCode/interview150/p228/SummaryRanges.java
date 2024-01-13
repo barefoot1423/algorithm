1a0e47163b6ec2fac283d55528d1bccf67a37a16
Hyunwoo Cho <barefoot1423@gmail.com>
2024-01-13 11:23:55 -0500

    done leetcode p228

--- src/main/java/com/hyunwoo/leetCode/interview150/p228/SummaryRanges.java
+++ src/main/java/com/hyunwoo/leetCode/interview150/p228/SummaryRanges.java
@@ -1,9 +1,51 @@
 package com.hyunwoo.leetCode.interview150.p228;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SummaryRanges {
+
     public List<String> summaryRanges(int[] nums) {
-        return null;
+
+        List<String> arrayList = new ArrayList<>();
+
+        if (nums.length == 0) {
+            return arrayList;
+        }
+
+        int lastNum = nums[nums.length - 1];
+
+        boolean isConsecutive = false;
+        StringBuilder ret = new StringBuilder();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != lastNum) {
+                if (nums[i] + 1 == nums[i + 1] && !isConsecutive) {
+                    ret.append(nums[i]);
+                    isConsecutive = true;
+                } else if (nums[i] + 1 != nums[i + 1] && !isConsecutive) {
+                    ret.append(nums[i]);
+                    arrayList.add(ret.toString());
+                    ret = new StringBuilder();
+                    isConsecutive = false;
+                } else if (nums[i] + 1 != nums[i + 1] && isConsecutive) {
+                    ret.append("->" + nums[i]);
+                    arrayList.add(ret.toString());
+                    ret = new StringBuilder();
+                    isConsecutive = false;
+                }
+            } else {
+                if (isConsecutive) {
+                    ret.append("->" + nums[i]);
+                    arrayList.add(ret.toString());
+                } else {
+                    ret.append(nums[i]);
+                    arrayList.add(ret.toString());
+                }
+            }
+
+        }
+
+        return arrayList;
     }
+
 }
---
+++ src/test/java/com/hyunwoo/leetCode/interview150/p228/SummaryRangesTest.java
@@ -0,0 +1,34 @@
+package com.hyunwoo.leetCode.interview150.p228;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SummaryRangesTest {
+
+    @Test
+    void summaryRanges() {
+        SummaryRanges summaryRanges = new SummaryRanges();
+        List<String> list1 = new ArrayList<>();
+        list1.add("0->2");
+        list1.add("4->5");
+        list1.add("7");
+//        assertEquals(list1, summaryRanges.summaryRanges(new int[] {0, 1, 2, 4, 5, 7}));
+
+        List<String> list2 = new ArrayList<>();
+        list2.add("0");
+        list2.add("2->4");
+        list2.add("6");
+        list2.add("8->9");
+//        assertEquals(list2, summaryRanges.summaryRanges(new int[] {0, 2, 3, 4, 6, 8, 9}));
+
+
+        List<String> list3 = new ArrayList<>();
+        assertEquals(list3, summaryRanges.summaryRanges(new int[] {}));
+    }
+
+}
