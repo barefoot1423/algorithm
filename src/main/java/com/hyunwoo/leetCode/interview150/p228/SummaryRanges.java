99b6cdaf2e859f84ee5f6d68936d606d4dd8a52d
Hyunwoo Cho <barefoot1423@gmail.com>
2024-01-12 21:12:59 -0500

    start leetcode p228

---
+++ src/main/java/com/hyunwoo/leetCode/interview150/p228/SummaryRanges.java
@@ -0,0 +1,9 @@
+package com.hyunwoo.leetCode.interview150.p228;
+
+import java.util.List;
+
+public class SummaryRanges {
+    public List<String> summaryRanges(int[] nums) {
+        return null;
+    }
+}
