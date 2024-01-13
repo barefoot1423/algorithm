8c081f54aa95aec2a1abefee8550ef67142a5988
Hyunwoo Cho <barefoot1423@gmail.com>
2024-01-13 11:24:22 -0500

    commit

--- src/test/java/com/hyunwoo/leetCode/interview150/p228/SummaryRangesTest.java
+++ src/test/java/com/hyunwoo/leetCode/interview150/p228/SummaryRangesTest.java
@@ -17,16 +17,15 @@ class SummaryRangesTest {
         list1.add("0->2");
         list1.add("4->5");
         list1.add("7");
-//        assertEquals(list1, summaryRanges.summaryRanges(new int[] {0, 1, 2, 4, 5, 7}));
+        assertEquals(list1, summaryRanges.summaryRanges(new int[] {0, 1, 2, 4, 5, 7}));
 
         List<String> list2 = new ArrayList<>();
         list2.add("0");
         list2.add("2->4");
         list2.add("6");
         list2.add("8->9");
-//        assertEquals(list2, summaryRanges.summaryRanges(new int[] {0, 2, 3, 4, 6, 8, 9}));
-
-
+        assertEquals(list2, summaryRanges.summaryRanges(new int[] {0, 2, 3, 4, 6, 8, 9}));
+        ˚
         List<String> list3 = new ArrayList<>();
         assertEquals(list3, summaryRanges.summaryRanges(new int[] {}));
     }
