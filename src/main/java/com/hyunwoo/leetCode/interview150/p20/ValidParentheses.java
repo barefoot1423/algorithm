4e165c59800c49d55f853fd2c9741b62e4e4dbad
Hyunwoo Cho <barefoot1423@gmail.com>
2024-01-14 22:51:55 -0500

    start leetcode p20

---
+++ src/main/java/com/hyunwoo/leetCode/interview150/p20/ValidParentheses.java
@@ -0,0 +1,9 @@
+package com.hyunwoo.leetCode.interview150.p20;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+
+        return false;
+    }
+
+}
---
+++ src/test/java/com/hyunwoo/leetCode/interview150/p20/ValidParenthesesTest.java
@@ -0,0 +1,19 @@
+package com.hyunwoo.leetCode.interview150.p20;
+
+import com.hyunwoo.leetCode.validParentheses.ValidParentheses;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidParenthesesTest {
+
+    @Test
+    void isValid() {
+        var validParentheses = new ValidParentheses();
+
+        assertEquals(true, validParentheses.isValid("()[]{}"));
+        assertEquals(false, validParentheses.isValid("[}"));
+        assertEquals(true, validParentheses.isValid("()"));
+    }
+
+}
