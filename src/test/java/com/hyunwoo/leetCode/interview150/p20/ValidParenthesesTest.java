ba418294c2490941d65baa73d05f934a3b7f4db0
Hyunwoo Cho <barefoot1423@gmail.com>
2024-01-15 16:10:30 -0500

    fix wrong import

--- src/test/java/com/hyunwoo/leetCode/interview150/p20/ValidParenthesesTest.java
+++ src/test/java/com/hyunwoo/leetCode/interview150/p20/ValidParenthesesTest.java
@@ -1,6 +1,5 @@
 package com.hyunwoo.leetCode.interview150.p20;
 
-import com.hyunwoo.leetCode.validParentheses.ValidParentheses;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
