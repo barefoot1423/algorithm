ecf64abb83e5715d3045ce2f2d721cfe6fc691a1
Hyunwoo Cho <barefoot1423@gmail.com>
2024-02-19 16:29:32 -0500

    done maximum depth of BST

--- src/main/java/com/hyunwoo/leetCode/interview150/p104/MaximumDepthOfBinaryTree.java
+++ src/main/java/com/hyunwoo/leetCode/interview150/p104/MaximumDepthOfBinaryTree.java
@@ -3,7 +3,15 @@ package com.hyunwoo.leetCode.interview150.p104;
 public class MaximumDepthOfBinaryTree {
     public int maxDepth(TreeNode root) {
 
-        return 0;
+        if (root == null) {
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return Math.max(left, right) + 1;
+
     }
 
 }
