e22685b827d88822a25ba0fe699abbeb8193167c
Hyunwoo Cho <barefoot1423@gmail.com>
2024-02-19 16:44:47 -0500

    done p100ˆ

--- src/main/java/com/hyunwoo/leetCode/interview150/p100/SameTree.java
+++ src/main/java/com/hyunwoo/leetCode/interview150/p100/SameTree.java
@@ -3,7 +3,19 @@ package com.hyunwoo.leetCode.interview150.p100;
 public class SameTree {
     public boolean isSameTree(TreeNode p, TreeNode q) {
 
-        return false;
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 
 }
