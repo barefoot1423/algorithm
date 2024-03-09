08aa80f9908b8e4a6b1131a234e459adc0cdb5fb
Hyunwoo Cho <barefoot1423@gmail.com>
2024-03-09 00:21:23 -0500

    commit

--- src/main/java/com/hyunwoo/leetCode/interview150/p35/SearchInsertPosition.java
+++ src/main/java/com/hyunwoo/leetCode/interview150/p35/SearchInsertPosition.java
@@ -2,8 +2,7 @@ package com.hyunwoo.leetCode.interview150.p35;
 
 public class SearchInsertPosition {
 
-    public int searchInsert(int[] nums, int target) {
-
+    public int searchInsert(int[] nums, int target) {kp
         return 0;
     }
 
