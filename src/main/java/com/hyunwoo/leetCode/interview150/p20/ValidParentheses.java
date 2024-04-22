2390c5a7f8d81e4522c4609ffc33814837519e13
Hyunwoo Cho <barefoot1423@gmail.com>
2024-04-21 23:26:38 -0400

    ˆ

--- src/main/java/com/hyunwoo/leetCode/interview150/p20/ValidParentheses.java
+++ src/main/java/com/hyunwoo/leetCode/interview150/p20/ValidParentheses.java
@@ -10,6 +10,7 @@ public class ValidParentheses {
 
         char[] sCharArray = s.toCharArray();
 
+
         for (int i = 0; i < sCharArray.length; i++) {
             if (sCharArray[i] == '(' || sCharArray[i] == '{' || sCharArray[i] == '[') {
                 stack.add(sCharArray[i]);
