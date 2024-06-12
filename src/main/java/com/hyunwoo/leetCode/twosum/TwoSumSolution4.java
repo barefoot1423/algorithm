64f49f14f8dd726a5d843ae37e1b873c97bb9dd5
Hyunwoo Cho <barefoot1423@gmail.com>
2024-06-12 00:25:40 -0400

    two sum solution in one pass

---
+++ src/main/java/com/hyunwoo/leetCode/twosum/TwoSumSolution4.java
@@ -0,0 +1,18 @@
+package com.hyunwoo.leetCode.twosum;
+
+import java.util.HashMap;
+
+public class TwoSumSolution4 {
+
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            var complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
